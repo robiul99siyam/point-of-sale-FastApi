a28d7c8d297c34697b311a15547e5b103e961473
robiul99siyam <siyamrobiulislam@gmail.com>
2025-02-08 19:42:03 +0600

    data base update here

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import uvicorn
 from fastapi import FastAPI 
-from database import  engine
+from database import engine
 import models
 from fastapi.middleware.cors import CORSMiddleware
-from routers import users,suppliers,category,customer,products,transaction,cash,login,dayClosed
+from routers import users, suppliers, category, customer, products, transaction, cash, login, dayClosed
 import os
 from fastapi.staticfiles import StaticFiles
 
@@ -15,8 +16,8 @@ app.add_middleware(
         "http://localhost:5173",
     ],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 UPLOAD_DIR = "uploads"
@@ -25,16 +26,18 @@ if not os.path.exists(UPLOAD_DIR):
 
 app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
 
-
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(users.routers)
-app.include_router(suppliers.routers)
-app.include_router(category.routers)
-app.include_router(customer.routers)
-app.include_router(products.routers)
-app.include_router(transaction.routers)
-app.include_router(cash.routers)
-app.include_router(login.routers)
-app.include_router(dayClosed.routers)
+app.include_router(users.router)
+app.include_router(suppliers.router)
+app.include_router(category.router)
+app.include_router(customer.router)
+app.include_router(products.router)
+app.include_router(transaction.router)
+app.include_router(cash.router)
+app.include_router(login.router)
+app.include_router(dayClosed.router)
 
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Use PORT env variable for Render
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
