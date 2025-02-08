cf2728707243b07cee122b5b6350f33165f87cec
robiul99siyam <siyamrobiulislam@gmail.com>
2025-02-08 19:24:30 +0600

    update database

--- database.py
+++ database.py
@@ -8,13 +8,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+# DB_USERNAME = os.getenv("DB_USERNAME")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+# DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:12345@localhost:5432/POINT_OF_SALES"
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./DD.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
